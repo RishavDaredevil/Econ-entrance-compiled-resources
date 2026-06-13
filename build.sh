4e8d84e5a0e891100ccfd33c2ee72d88f364bdc7
Rishav Dhariwal <sam.rishav2002@gmail.com>
2026-06-13 06:21:12 +0000

    refactor: rename resource dir, update indian econ headings and labels

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p site_build
-cp -r "Stuff to add in directory/Special" "site_build/"
-cp -r "Stuff to add in directory/Econ-entrance-more-resources" "site_build/"
+cp -r "All_resources/Special" "site_build/"
+cp -r "All_resources/Econ-entrance-more-resources" "site_build/"
 echo "Assets copied to site_build successfully."
